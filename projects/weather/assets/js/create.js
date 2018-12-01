e7318bc9d6b014e1e30edceeae37552da1baedea
Abhishek Shastri <shastri48@gmail.com>
2018-12-01 22:30:12 +0530

    add weather project

--- projects/weather/assets/js/create.js
+++
@@ -1,6 +0,0 @@
-export function create(tag, content,attribute, parent){
-  var tagName = document.createElement(tag);
-  tagName.setAttribute("id", attribute);
-  tagName.textContent = content;
-  parent.appendChild(tagName);  
-}
